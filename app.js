078d98bbb9b5171a712279505cc63f94cbbc86ea
vaibhav <vaibh>
2022-08-09 23:23:59 +0530

    first

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ app.post("/failure",function(req,res){
 
 
 
-app.listen(process.env.PORT, function () {
+app.listen(process.env.PORT||3000, function () {
     console.log("server is running at port 3000")
 })
 
